ca7a1694aaa26c52c1e6db1d7f632b8c76de30ad
rapid-dishonesty <hrj41bib@gmail.com>
2025-03-09 10:43:01 +0000

    Add files via upload

--- frogger-project.py
+++ frogger-project.py
@@ -1,6 +1,8 @@
 from pygame import *
+from random import *
 
 init()
+
 width = 600
 height = 400
 screen = display.set_mode((width, height))
@@ -9,12 +11,19 @@ endGame = False
 
 frogPic = image.load("frog.JPG")
 frogPic = transform.scale(frogPic, (40,40))
+
 carPic = image.load("car.JPG")
-carPic = transform.scale(carPic, (75,40))
+carPic = transform.scale(carPic, (70,35))
+
 curb1 = Rect(0, height - 50, width, 10)
 curb2 = Rect(0, height - 170, width, 10)
-
 frogRect = Rect (width // 2 - 20,height - 40,40,40)
+
+road_top = curb2.bottom
+road_bottom = curb1.top - 40
+
+cars = [Rect(randint(600, 800), road_top + 10,70,35), Rect(randint(600, 800),road_top + 60, 70, 35), Rect(randint(600, 800),road_bottom - 60, 70, 35)]
+cars.append(Rect(350,275,70,35))
 
 mixer.music.load("coolsong.mp3")
 mixer.music.play(-1)
@@ -23,19 +32,37 @@ while endGame == False:
     for e in event.get():
         if e.type == QUIT:
             endGame = True
+
         if e.type == KEYDOWN:
-            if e.key == K_s:
+            if e.key == K_DOWN:
                 frogRect.move_ip(0,40)
-            if e.key == K_w:
+            if e.key == K_UP:
                 frogRect.move_ip(0,-40)
-            if e.key == K_a:
+            if e.key == K_LEFT:
                 frogRect.move_ip(-40,0)
-            if e.key == K_d:
+            if e.key == K_RIGHT:
                 frogRect.move_ip(40,0)
+
+    for car in cars:
+        car.x -= randint(3, 6)  # Cars move at slightly different speeds
+        if car.x < -75:  # If car leaves screen, reset to right side
+            car.x = randint(600, 800)
+
+    for c in cars:
+        if c.colliderect(frogRect):
+            print("you're dead, oopsie")
+            endGame = True
+
     screen.fill((0,0,0))
+    for c in cars:
+      draw.rect(screen, (255,255,255),c)
     draw.rect(screen, (255,255,255), (0,0,width,20))
+
+    for car in cars:
+        screen.blit(carPic, car)
+
     screen.blit(frogPic, frogRect)
-    screen.blit(carPic, (350,275))
+    #screen.blit(carPic, (350,275))
     draw.rect(screen, (128, 128, 128), curb1)
     draw.rect(screen, (128, 128, 128), curb2)
     display.update()
